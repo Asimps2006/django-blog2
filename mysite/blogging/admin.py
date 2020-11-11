975289c3ada545e0140267cb1eb3bfa1696349e4
Andy Simpson <asimps2006@gmail.com>
2020-11-10 19:12:11 -0800

    Assignment07 - Add Categories to blog Post creation page by modifying admin.py  - AS

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,7 +1,18 @@
 # a new import
 from django.contrib import admin
 from blogging.models import Post, Category
+from django.db import models
+
 
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+# Define an inline class here
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = (CategoryInLine,)
